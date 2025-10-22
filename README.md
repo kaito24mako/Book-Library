e094d2319ebb6e30e03753e94b6b491aa7d4d0f3
kaito24mako <kaitowatanabemcc@gmail.com>
2025-10-22 15:17:27 +1100

    create functions for book constructor and adding book to an array (library)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Library App
+As instructed by The Odin Project, this library app will utilise the intermediate object and object constructor skills I've learned.
+
+## Skills 
+**JavaScript**
---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+addBookToLibrary("1984", "George Orwell", "182", "read");
+console.log(myLibrary);
