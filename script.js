5954338a097274e0adf6fa8bfb817be261bd187a
kaito24mako <kaitowatanabemcc@gmail.com>
2025-10-23 21:21:40 +1100

    create dom elements

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 addBookToLibrary("1984", "George Orwell", "182", "read");
 console.log(myLibrary);
+
+
+const cardContainer = document.querySelector(".card-container");
+
+function getBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+        cardContainer.appendChild(newCard);
+
+        const textGroup = document.createElement("div");
+        textGroup.classList.add("card-text");
+        newCard.appendChild(textGroup);
+
+        const titleText = document.createElement("h2");
+        const authorText = document.createElement("p");
+        const pagesText = document.createElement("p");
+        const readText = document.createElement("p");
+        textGroup.appendChild(titleText, authorText, pagesText, readText);
+
+        const buttonsGroup = document.createElement("div");
+        buttonsGroup.classList.add("card-buttons");
+        newCard.appendChild(buttonsGroup);
+
+        const statusButton = document.createElement("button");
+        const removeButton = document.createElement("button");
+        buttonsGroup.appendChild(statusButton, removeButton);
+    }
+}
