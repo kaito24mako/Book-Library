77ba739589048a4351a26fe4bb596f0d51f3e06b
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-04 17:15:44 +1100

    move functions and events into an IIFE for clean coding

--- script.js
+++ script.js
@@ -1,129 +1,140 @@
-/* Variables */
+const libraryApp = (() => {
 
-let myLibrary = [];
+    /* ==== Variables ==== */
 
-const cardContainer = document.querySelector(".card-container");
+    let myLibrary = [];
 
-const dialog = document.querySelector("dialog");
-const createBookButton = document.querySelector("#new-book-button");
-const closeDialogButton = document.querySelector("#close-dialog-button");
+    const cardContainer = document.querySelector(".card-container");
 
-const form = document.querySelector(".form-container");
-const titleInput = form.elements["title"];
-const authorInput = form.elements["author"];
-const pagesInput = form.elements["pages"];
-const readInput = form.elements["read-status"];
+    const dialog = document.querySelector("dialog");
+    const createBookButton = document.querySelector("#new-book-button");
+    const closeDialogButton = document.querySelector("#close-dialog-button");
 
-/* Book constructor */
+    const form = document.querySelector(".form-container");
+    const titleInput = form.elements["title"];
+    const authorInput = form.elements["author"];
+    const pagesInput = form.elements["pages"];
+    const readInput = form.elements["read-status"];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = `By ${author}`;
-    this.pages = `${pages} pages`;
-    this.read = `Status: ${read}`;
-    this.id = crypto.randomUUID();
-};
+    /* ==== Book Constructor ==== */
 
-Book.prototype.toggleReadStatus = function() {
-    if (this.read === "Status: Completed") {
-        this.read = "Status: In Progress";
-    } else {
-        this.read = "Status: Completed";
-    };
-};
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = `By ${author}`;
+        this.pages = `${pages} pages`;
+        this.read = `Status: ${read}`;
+        this.id = crypto.randomUUID();
+    }
 
-/* Add new books to the library array */
+    Book.prototype.toggleReadStatus = function() {
+        this.read = this.read === "Status: Completed" ? "Status: In Progress" : "Status: Completed";
+    }
 
-function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
+    /* ==== Add Books to Library Array ==== */
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "310", "In Progress");
-addBookToLibrary("1984", "George Orwell", "336", "Completed");
-addBookToLibrary("The Alchemist", "Paulo Coelho", "208", "Completed");
-addBookToLibrary("War and Peace", "Leo Tolstoy", "1225", "In Progress");
+    function addBookToLibrary(title, author, pages, read) {
+        let newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+    }
 
-/* Display books */
+    /* ==== Display Books ==== */
 
-function getBook(array) {
-    for (let i = 0; i < myLibrary.length; i++) {
+    function displayBook(array) {
+        for (let i = 0; i < myLibrary.length; i++) {
 
-        /* create and append card elements */
-        const newCard = document.createElement("div");
-        newCard.classList.add("card");
-        cardContainer.appendChild(newCard);
+            /* create and append card elements */
+            const newCard = document.createElement("div");
+            newCard.classList.add("card");
+            cardContainer.appendChild(newCard);
 
-        const textGroup = document.createElement("div");
-        textGroup.classList.add("card-text");
-        newCard.appendChild(textGroup);
+            const textGroup = document.createElement("div");
+            textGroup.classList.add("card-text");
+            newCard.appendChild(textGroup);
 
-        const titleText = document.createElement("h2");
-        const authorText = document.createElement("p");
-        const pagesText = document.createElement("p");
-        const readText = document.createElement("p");
-        titleText.textContent = array[i].title;
-        authorText.textContent = array[i].author;
-        pagesText.textContent = array[i].pages;
-        readText.textContent = array[i].read;
-        textGroup.append(titleText, authorText, pagesText, readText);
-        
-        const buttonsGroup = document.createElement("div");
-        buttonsGroup.classList.add("card-buttons");
-        newCard.appendChild(buttonsGroup);
+            const titleText = document.createElement("h2");
+            const authorText = document.createElement("p");
+            const pagesText = document.createElement("p");
+            const readText = document.createElement("p");
+            titleText.textContent = array[i].title;
+            authorText.textContent = array[i].author;
+            pagesText.textContent = array[i].pages;
+            readText.textContent = array[i].read;
+            textGroup.append(titleText, authorText, pagesText, readText);
+            
+            const buttonsGroup = document.createElement("div");
+            buttonsGroup.classList.add("card-buttons");
+            newCard.appendChild(buttonsGroup);
 
-        const statusButton = document.createElement("button");
-        const removeButton = document.createElement("button");
-        statusButton.dataset.id = array[i].id;
-        removeButton.dataset.id = array[i].id;
-        statusButton.textContent = "Change Read Status";
-        removeButton.textContent = "Remove Book";
-        buttonsGroup.append(statusButton, removeButton);
+            const statusButton = document.createElement("button");
+            const removeButton = document.createElement("button");
+            statusButton.dataset.id = array[i].id;
+            removeButton.dataset.id = array[i].id;
+            statusButton.textContent = "Change Read Status";
+            removeButton.textContent = "Remove Book";
+            buttonsGroup.append(statusButton, removeButton);
 
-        /* change read status */
-        statusButton.addEventListener("click", () => {
-            const currentBook = myLibrary.find(book => book.id === statusButton.dataset.id);
-            currentBook.toggleReadStatus();
-            readText.textContent = currentBook.read;
+            /* change read status */
+            statusButton.addEventListener("click", () => {
+                const currentBook = myLibrary.find(book => book.id === statusButton.dataset.id);
+                currentBook.toggleReadStatus();
+                readText.textContent = currentBook.read;
+            });
+
+            /* remove book */
+            removeButton.addEventListener("click", () => {
+                myLibrary = myLibrary.filter(book => book.id !== removeButton.dataset.id);
+                cardContainer.innerHTML = "";
+                displayBook(myLibrary);
+            });
+        };
+    }
+
+    /* ==== Event Listeners ==== */
+
+    function initEventListeners() {
+        createBookButton.addEventListener("click", () => {
+            dialog.showModal();
         });
 
-        /* remove book */
-        removeButton.addEventListener("click", () => {
-            myLibrary = myLibrary.filter(book => book.id !== removeButton.dataset.id);
+        closeDialogButton.addEventListener("click", () => {
+            dialog.close();
+            form.reset();
+        });
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            
+            /* add new book to array */
+            addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+
+            /* display the new book */
             cardContainer.innerHTML = "";
-            getBook(myLibrary);
+            displayBook(myLibrary);
+
+            /* close and reset form */
+            dialog.close();
+            form.reset();
         });
-    };
-};
+    }
 
-getBook(myLibrary);
+    /* ==== Inititalise App ==== */
 
-/* Display the new book form */
+    function init() {
+        addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "310", "In Progress");
+        addBookToLibrary("1984", "George Orwell", "336", "Completed");
+        addBookToLibrary("The Alchemist", "Paulo Coelho", "208", "Completed");
+        addBookToLibrary("War and Peace", "Leo Tolstoy", "1225", "In Progress");
 
-createBookButton.addEventListener("click", () => {
-    dialog.showModal();
-});
+        displayBook(myLibrary);
+        initEventListeners();
+    }
 
-closeDialogButton.addEventListener("click", () => {
-    dialog.close();
-    form.reset();
-});
+    return {init};
 
-/* Values of the form create and display a new book */
+})()
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    /* add new book (using the values of the form inputs) to array */
-    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
-    /* display the new book */
-    cardContainer.innerHTML = "";
-    getBook(myLibrary);
+libraryApp.init();
 
-    /* close and reset form */
-    dialog.close();
-    form.reset();
-});
 
 
 
